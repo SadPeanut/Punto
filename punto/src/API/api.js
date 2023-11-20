6aa8316043d6e62056a686f23ffd2eceae2fc973
SadPeanut <slanistas29@gmail.com>
2023-11-20 15:29:32 +0100

    sqlite3

--- punto/src/API/api.js
+++ punto/src/API/api.js
@@ -1,8 +1,8 @@
 const MySQL_SPDO = require('./MySQL_SPDO'); // Remplacez './MySQL_SPDO' par le chemin correct du fichier
 const MongoDB_SPDO = require('./MongoDB_SPDO'); // Assurez-vous de spécifier le chemin correct
+const SQLite_SPDO = require('./SQLite_SPDO'); // Assurez-vous de spécifier le chemin correct
 
-
-async function insertGameData1(gameData) {
+async function insertPartieMySQL(gameData) {
     const db = MySQL_SPDO.getInstance();
 
     try {
@@ -27,7 +27,7 @@ async function insertGameData1(gameData) {
 }
 
 
-async function insertGameData(gameData) {
+async function insertPartieMongoDB(gameData) {
     const db = MongoDB_SPDO.getInstance();
     
     try {
@@ -40,10 +40,32 @@ async function insertGameData(gameData) {
 
 
 
+async function insertPartieSQLite(gameData){
+    const db = SQLite_SPDO.getInstance('C:/Users/slani/OneDrive/Desktop/BDDs_Punto/BDD_Sqlite/BDD_Punto.db');
+
+    try {
+        const insertQuery = `
+            INSERT INTO Partie 
+            (id_joueur_gagnant, manches_gagnees, points_joueur1, points_joueur2, points_joueur3, points_joueur4) 
+            VALUES (?, ?, ?, ?, ?, ?)
+        `;
+        const result = await db.query(insertQuery, [
+            gameData.id_joueur_gagnant,
+            gameData.manches_gagnees,
+            gameData.points_joueur1,
+            gameData.points_joueur2,
+            gameData.points_joueur3,
+            gameData.points_joueur4
+        ]);
+
+        console.log('Lignes affectées :', result);
+    }catch(err){
+        console.error(`Erreur lors de l'insertion des données : ${err}`);
+    }
+}
 
 
 
-// Exemple d'utilisation de la fonction pour insérer des données
 const gameData = {
     id_joueur_gagnant: 1,
     manches_gagnees: 2,
@@ -53,7 +75,6 @@ const gameData = {
     points_joueur4: 5
 };
 
-//insertGameData(gameData);
-//insertGameData1(gameData);
+insertPartieSQLite(gameData);
 
 
